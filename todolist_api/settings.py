258d3c07f17d399c12a0952684c1b2bcb6b43381
Bari <134767175+Khroxx@users.noreply.github.com>
2024-06-25 11:07:20 +0200

    Update settings.py

--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zn)0&csw#l3e*@opjk(!s-^)6af*nxrzeal+sx)%n5q)0-ql)y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['Bari.pythonanywhere.com']
+ALLOWED_HOSTS = ['Bari.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
