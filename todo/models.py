f51f762821f78e6c437b5a7684ab007be669446e
Bari <134767175+Khroxx@users.noreply.github.com>
2024-06-26 11:11:26 +0200

    model+ todo + rest framework

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-from django.conf import settings
+import datetime
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 class Todo(models.Model):
     title = models.CharField(max_length=20)
     description = models.CharField(max_length=100)
-    created_at = models.DateField(auto_now_add=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateField(default=datetime.date.today)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .models import Todo
+from rest_framework import permissions, viewsets
+from .serializers import TodoSerializer
 
 # Create your views here.
 
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Todo.objects.all().order_by('-created_at')
+    serializer_class = TodoSerializer
+    permission_classes = [] # permissions.IsAuthenticated
 
-def index(request):
-    return render(request, 'index.html')
-
-def redirect(request):
-    return redirect('index/')
